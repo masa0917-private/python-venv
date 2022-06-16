91972e35e4fdfda0f92437f6e43a30dc2a63be0b
ZCmasa <zc.masaya.211101@gmail.com>
2022-06-16 11:31:05 +0900

    streaming-improvement

--- choco-tei-mini/frame_streaming.py
+++ choco-tei-mini/frame_streaming.py
@@ -1,19 +1,46 @@
 # coding: UTF-8
 
 # from bs4 import BeautifulSoup
+from requests import options
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import Service as ChromeService
 import time
+from bs4 import BeautifulSoup
+
+REFRESH_RATE = 0.1
 
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+service = ChromeService(executable_path=ChromeDriverManager().install())
+
+# # ブラウザを非表示にするときに使用
+# options = webdriver.ChromeOptions()
+# options.add_argument("--headless")
+# driver = webdriver.Chrome(service=service, options=options)
+
+# ブラウザ(Chrome)が起動するように選択
+driver = webdriver.Chrome(service=service)
+
+# 画像データURLを抽出後、保存するための箱
+src = []
 
 try:
     while True:
         driver.get("http://192.168.178.178/xaccja/getCamImage")
-        time.sleep(0.01)
+
+        html = driver.page_source.encode('utf-8')
+        soup = BeautifulSoup(html, 'lxml')
+        results = soup.find("img")
+        src.append(results.get("src"))
+
+        print('===== src =====')
+        print(len(src))
+        print('===============')
+
+        time.sleep(REFRESH_RATE)
 except KeyboardInterrupt:
+    # ctrl+c で終了
+    driver.quit()
     print('--- Finish ---')
 
 # html = driver.page_source.encode('utf-8')
