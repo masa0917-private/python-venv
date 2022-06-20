8936d18dd37b745a1ece0585bfba93ab35722bba
ZCmasa <zc.masaya.211101@gmail.com>
2022-06-20 12:28:03 +0900

    used streamlit

---
+++ axross/stapp/chat-bot-with-streamlit.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import requests
+
+apikey = "DZZKE1KUGVnRcgt9JpdN06QHaCxmpaM9"
+
+chat_logs = []
+
+st.title('Chatbot with streamlit')
+st.subheader('メッセージを入力してから送信をタップしてください')
+message = st.text_input('メッセージ')
+
+def send_pya3rt(endpoint, apikey, text, callback):
+    params = {'apikey': apikey,
+                    'query' : text,
+                    }
+    if callback is not None:
+        params['callback'] = callback
+
+    response = requests.post(endpoint, params)
+
+    return response.json()
+
+def generate_response():
+    ans_json = send_pya3rt('https://api.a3rt.recruit.co.jp/talk/v1/smalltalk', apikey, message, None)
+    ans = ans_json['results'][0]['reply']
+    chat_logs.append('you: ' + message)
+    chat_logs.append('AI: ' + ans)
+    for chat_log in chat_logs:
+        st.write(chat_log)
+
+if st.button('送信'):
+    generate_response()
